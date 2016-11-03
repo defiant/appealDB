d1ba26cab04687ff6e88e9bc848bbc44d857ff9d
Sinan Taga <sinantaga@gmail.com>
2016-11-03 23:06:49 +0200

    add disqus and analytics

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -167,6 +167,29 @@
                 </small>
             </div>
         </div>
+
+        <div class="columns">
+            <div class="column is-10 is-offset-1">
+                <hr>
+                <h2 class="title">Discussion</h2>
+                <div id="disqus_thread"></div>
+                <script>
+                    /*
+                    var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+                    */
+                    (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = '//appealdb.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            </div>
+        </div>
     </div>
-{{--{{dd($appeal)}}--}}
+
 @endsection
