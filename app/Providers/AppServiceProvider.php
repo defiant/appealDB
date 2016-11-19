fdb472b0844aa9248478f63282118fbb870ecb26
Sinan Taga <sinantaga@gmail.com>
2016-11-20 00:03:40 +0300

    Fix escaped markup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::directive('markdown', function($expression){
-            return "<?php echo app('markdown')->text(e({$expression}));?>";
+            return "<?php echo app('markdown')->setMarkupEscaped(true)->text({$expression});?>";
         });
     }
 
